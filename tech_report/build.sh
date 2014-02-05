70e3ed37c0c1abc2cd08f780b25a98e69ec87ec8
Pedro Borges <highvolt16@gmail.com>
2014-02-05 09:52:25 +0000

    The beginning

---
+++ tech_report/build.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Remove existent PDF file.
+rm pdis-en.pdf 2> /dev/null
+
+# Build report.
+if [[ "$#" -eq 1 ]]; then
+
+  if [[ "$1" =~ s|S ]]; then
+    make pdis-en._show
+  else
+    echo "Invalid argument"
+  fi
+
+else
+  make pdis-en
+fi
+
+# Clean compilation files.
+make clean
